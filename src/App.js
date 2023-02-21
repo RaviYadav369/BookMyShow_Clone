6c5b2ea60b2e41e6a48cf3d2ace083ad3b3161d9
Ravi Yadav <shortanswer12345@gmail.com>
2023-02-21 12:19:18 +0530

    add movie page

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ import "slick-carousel/slick/slick-theme.css";
 
 import ExplorePage from './navigation-pages/Explore-Page';
 import Home from './navigation-pages/Home';
+import FullMoviePage from './pages/Full-Movie-Page';
 
-axios.defaults.baseURL= "https://api.themoviedb.org/3";
-axios.defaults.params ={};
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
 axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 
 function App() {
@@ -22,11 +23,12 @@ function App() {
         <Route path='/:type/*' element={<ExplorePage />} /> */}
         <Route path='/explore' element={<Navigate to="/explore/home" />} />
         <Route path='/explore/:type' element={<Home />} />
-        
-        {/* <Route path='/movies:id' element={<MoviesFullPage />} />
-    <Route path='/events:id' element={<EventsFullPages />} />
-    <Route path='/play:id' element={<PlayFullPage />} />
-    <Route path='/sports:id' element={<SportsFullPage />} /> */}
+
+        <Route path='/movie/:id' element={<FullMoviePage />} />
+
+        {/* <Route path='/events:id' element={<EventsFullPages />} />
+        <Route path='/play:id' element={<PlayFullPage />} />
+        <Route path='/sports:id' element={<SportsFullPage />} /> */}
       </Routes>
     </>
   );
--- src/pages/Event-Page.jsx
+++ src/pages/Event-Page.jsx
@@ -12,7 +12,7 @@ const EventPage = () => {
       <div className="container mx-auto px-4 my-10">
         <div className="w-full flex flex-row-reverse lg:flex-row-reverse gap-4 ssm:flex-col-reverse">
           <div className="lg:w-3/4 p-4 bg-white rounded">
-            <h2 className="text-2xl font-bold mb-4">Plays in NCR Delhi</h2>
+            <h2 className="text-2xl font-bold mb-4">Events in NCR Delhi</h2>
             <div className="flex flex-wrap ">
             <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center ssm:w-full">
                 <Card
